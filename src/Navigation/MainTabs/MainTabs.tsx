e9722d2894bbe32ef1d11e94de6ae79485380d77
ahmedabdalla67 <ahmedabdalla6728@gmail.com>
2026-05-21 15:51:51 +0300

    feat: Add Ionicons for tab bar icons in MainTabs and update package dependencies

--- src/Navigation/MainTabs/MainTabs.tsx
+++ src/Navigation/MainTabs/MainTabs.tsx
@@ -2,14 +2,38 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import RouteName from "../../utils/routes/RouteName";
 import HomeScreen from "../../screens/homeScreen/HomeScreen";
 import FavoriteScreen from "../../components/FavoriteArticles/FavoriteScreen";
+import React from "react";
+import Ionicons from "@react-native-vector-icons/ionicons";
 
 const Tab = createBottomTabNavigator();
 
 export default function MainTabs() { 
     return (
-        <Tab.Navigator>
-            <Tab.Screen name={RouteName.HomeScreen} component={HomeScreen} />
-            <Tab.Screen name={RouteName.FavoriteArticles} component={FavoriteScreen} />
+        <Tab.Navigator
+            screenOptions={{
+                headerShown: false
+            }}
+        >
+            <Tab.Screen
+                name={RouteName.HomeScreen} component={HomeScreen}
+                options={{
+                    tabBarLabel: "Home",
+                    tabBarIcon: ({ color }) => {
+                        <Ionicons name="home" size={24} color={color} />
+                    },
+                    tabBarActiveTintColor: "red"
+                }}
+            />
+            <Tab.Screen
+                name={RouteName.FavoriteArticles} component={FavoriteScreen}
+                options={{
+                    tabBarLabel: "Favorites",
+                    tabBarIcon: ({ color }) => {
+                        <Ionicons name="heart" size={24} color={color} />
+                    },
+                    tabBarActiveTintColor: "red"
+                }}
+            />
         </Tab.Navigator>
     )
 }
